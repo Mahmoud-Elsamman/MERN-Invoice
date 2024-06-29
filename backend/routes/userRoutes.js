03cf0bce3222fd049b6a923508279e1a8d5947d1
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2024-06-30 02:08:44 +0300

    feat: get all users accounts-admin only

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,8 @@ import checkAuth from "../Middleware/checkAuthMiddleware.js";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
 import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
+import role from "../Middleware/roleMiddleware.js";
+import getAllUserAccounts from "../controllers/user/getAllUsersAccounts.js";
 const router = express.Router();
 
 router
@@ -11,4 +13,7 @@ router
   .patch(checkAuth, updateUserProfile)
   .delete(checkAuth, deleteMyAccount);
 
+router
+  .route("/all")
+  .get(checkAuth, role.checkRole(role.ROLES.admin), getAllUserAccounts);
 export default router;
