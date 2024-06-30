63c1892e068f63c2513b745208b135b2f6abe8cc
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2024-06-30 03:24:36 +0300

    feat: deactivate user account

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
 import role from "../Middleware/roleMiddleware.js";
 import getAllUserAccounts from "../controllers/user/getAllUsersAccounts.js";
 import deleteUserAccount from "../controllers/user/deleteUserAccount.js";
+import deactivateUser from "../controllers/user/deactivateUser.js";
 const router = express.Router();
 
 router
@@ -21,4 +22,8 @@ router
 router
   .route("/:id")
   .delete(checkAuth, role.checkRole(role.ROLES.admin), deleteUserAccount);
+
+router
+  .route("/:id/deactivate")
+  .patch(checkAuth, role.checkRole(role.ROLES.admin), deactivateUser);
 export default router;
